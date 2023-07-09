c00b3b667c22dfc722ea2de05f0505601940faaf
joindar32 <125015919+joindar32@users.noreply.github.com>
2023-07-09 16:30:36 +0300

    2nd
    added portal

--- summer_practice/Assets/Movement.cs
+++ summer_practice/Assets/Movement.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+using UnityEngine;
+ 
 public enum Speeds { Slow = 0, Normal = 1, Fast = 2, Faster = 3, Fastest = 4 };
 public enum Gamemodes { Cube = 0, Ship = 1 };
+public enum Gravity { Upright = 1, Upsidedown = -1 };
+
 public class Movement : MonoBehaviour
 {
     public Speeds CurrentSpeed;
+    public Gamemodes CurrentGamemode;
     //                       0      1      2       3      4
     float[] SpeedValues = { 8.6f, 10.4f, 12.96f, 15.6f, 19.27f };
 
@@ -47,5 +53,21 @@ public class Movement : MonoBehaviour
     bool OnGround()
     {
         return Physics2D.OverlapCircle(GroundCheckTransform.position, GroundCheckRadius, GroundMask);
+    }
+
+    public void ChangeThroughPortal(Gamemodes Gamemode, Speeds Speed, Gravity gravity, int State)
+    {
+        switch (State)
+        {
+            case 0:
+                CurrentSpeed = Speed;
+                break;
+            case 1:
+                CurrentGamemode = Gamemode;
+                break;
+            case 2:
+                rb.gravityScale = Mathf.Abs(rb.gravityScale) * (int)gravity;
+                break;
+        }
     }
 }
